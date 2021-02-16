4fc3c3e9374f81567fe2bf989595a6845fb21d38
SleepingProgrammer <32633305+SleepingProgrammer@users.noreply.github.com>
2021-02-16 14:08:06 +0800

    Semi Stable version

---
+++ imports/ConnectionConfigurations.copy.js
@@ -0,0 +1,10 @@
+
+var ConnectionConfigurations = {};
+
+//TODO: RENAME FILE INTO ConnectionConfigurations.js then xSETUP EMAIL CONFIGURATION HERE
+ConnectionConfigurations.EMAIL = {};
+ConnectionConfigurations.EMAIL.username_1 = '';
+ConnectionConfigurations.EMAIL.password_1 = '';
+
+
+export default ConnectionConfigurations;
